b9323e9fd872c42e13d1001bea94c08a6bd016d3
David Faure <aswelldavid@gmail.com>
2020-06-24 17:59:09 +0200

    setup route ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const api = require("./routes");
 const bodyParser = require("body-parser");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 app.use(cors());
 
@@ -15,5 +15,5 @@ app.listen(port, (err) => {
   if (err) {
     throw new Error("There is an error");
   }
-  console.log(`Welcom to the port ${port}`);
+  console.log(`Welcome to the port ${port}`);
 });
